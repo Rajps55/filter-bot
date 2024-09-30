b73f5a984e92a5c78efda599a1445506f99aa4df
Rajps55 <hackinggm69@gmail.com>
2024-09-30 06:26:13 -0700

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from info import DATABASE_NAME, DATABASE_URL, IMDB_TEMPLATE, PM_SEARCH, WELCOME_TEXT, AUTH_CHANNEL, LINK_MODE, TUTORIAL, SHORTLINK_URL, SHORTLINK_API, SHORTLINK, FILE_CAPTION, IMDB, WELCOME, SPELL_CHECK, PROTECT_CONTENT, AUTO_FILTER, AUTO_DELETE, IS_STREAM, VERIFY_EXPIRE, IS_STREAM
+from info import DATABASE_NAME, DATABASE_URL, IMDB_TEMPLATE, WELCOME_TEXT, AUTH_CHANNEL, LINK_MODE, TUTORIAL, SHORTLINK_URL, SHORTLINK_API, SHORTLINK, FILE_CAPTION, IMDB, WELCOME, SPELL_CHECK, PROTECT_CONTENT, AUTO_FILTER, AUTO_DELETE, IS_STREAM
 import time
 import datetime
 
@@ -30,15 +30,13 @@ class Database:
         'is_verified': False,
         'verified_time': 0,
         'verify_token': "",
-        'link': "",
-        'expire_time': 0
+        'link': ""
     }
     
     def __init__(self):
         self.col = mydb.Users
         self.grp = mydb.Groups
         self.users = mydb.uersz
-        self.botcol = mydb["bot_id"]
 
     def new_user(self, id, name):
         return dict(
@@ -148,15 +146,7 @@ class Database:
     async def get_verify_status(self, user_id):
         user = await self.col.find_one({'id':int(user_id)})
         if user:
-            info = user.get('verify_status', self.default_verify)
-            try:
-                info.get('expire_time')
-            except:
-                expire_time = info.get('verified_time') + datetime.timedelta(seconds=VERIFY_EXPIRE)
-                info.append({
-                    'expire_time': expire_time
-                })
-            return info
+            return user.get('verify_status', self.default_verify)
         return self.default_verify
         
     async def update_verify_status(self, user_id, verify):
@@ -172,6 +162,7 @@ class Database:
     async def get_db_size(self):
         return (await mydb.command("dbstats"))['dataSize']
    
+    
     async def get_user(self, user_id):
         user_data = await self.users.find_one({"id": user_id})
         return user_data
@@ -212,29 +203,5 @@ class Database:
         expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
         user_data = {"id": user_id, "expiry_time": expiry_time, "has_free_trial": True}
         await self.users.update_one({"id": user_id}, {"$set": user_data}, upsert=True)
-
-    async def all_premium_users(self):
-        count = await self.users.count_documents({
-        "expiry_time": {"$gt": datetime.datetime.now()}
-        })
-        return count
-
-    async def get_pm_search_status(self, bot_id):
-        bot = await self.botcol.find_one({'id': bot_id})
-        if bot and bot.get('bot_pm_search'):
-            return bot['bot_pm_search']
-        else:
-            return PM_SEARCH
-
-    async def update_pm_search_status(self, bot_id, enable):
-        bot = await self.botcol.find_one({'id': int(bot_id)})
-        if bot:
-            await self.botcol.update_one({'id': int(bot_id)}, {'$set': {'bot_pm_search': enable}})
-        else:
-            await self.botcol.insert_one({'id': int(bot_id), 'bot_pm_search': enable})
-
-    async def get_all_chats_count(self):
-        grp = await self.grp.find().to_list(None)
-        return grp
         
 db = Database()
